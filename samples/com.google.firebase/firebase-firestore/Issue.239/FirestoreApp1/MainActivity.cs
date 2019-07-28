1b54161c24d93783d8166069ff7e177593b30a8a
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-07-28 11:11:18 +0300

    perf samples

---
+++ samples/com.google.firebase/firebase-firestore/Issue.239/FirestoreApp1/MainActivity.cs
@@ -0,0 +1,75 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Firebase.Firestore;
+using Android.Gms.Tasks;
+using Java.Lang;
+using System;
+
+namespace FirestoreApp1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnCompleteListener, IOnFailureListener
+    {
+        Firebase.FirebaseApp fireApp;
+        Firebase.Firestore.FirebaseFirestore fireFirestore;
+        Firebase.Database.FirebaseDatabase fireDatabase;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            // Well, just config with your own name app in manifest.xml
+            // and yout own "google-services.json" file config from Firebase. that's all.
+
+
+
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            fireApp = Firebase.FirebaseApp.InitializeApp(this);
+            fireFirestore = Firebase.Firestore.FirebaseFirestore.Instance;
+            fireDatabase = Firebase.Database.FirebaseDatabase.Instance;
+
+            Button btn1 = FindViewById(Resource.Id.Button1) as Button;
+            btn1.Click += delegate
+            {
+                
+                // So here it's crash the App.
+                
+                DocumentReference docRef = fireFirestore.Collection("collection1").Document("doc1");
+                docRef.Get()
+                    .AddOnCompleteListener(this)
+                    .AddOnFailureListener(this);
+            };
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void OnComplete(Task task)
+        {
+            if (task.IsSuccessful)
+            {
+                DocumentSnapshot doc = task.Result as DocumentSnapshot;
+                if (doc.Exists())
+                {
+                    Console.WriteLine($"Doc all read OK: {doc.Data}");
+                }
+            }
+        }
+
+        public void OnFailure(Java.Lang.Exception e)
+        {
+            Console.WriteLine($"Exception reading doc: {e.Message}");
+        }
+    }
+}
