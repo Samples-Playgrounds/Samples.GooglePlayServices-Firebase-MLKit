b28604fbf7ef9b057f70c3059881ad505fdef629
moljac <mcvjetko@holisticware.net>
2022-04-28 14:01:10 +0200

    issue 533

---
+++ samples/issues-bug-repros/com.google.firebase/000533-Firebase.InAppMessaging/AppTest/AppTest/AppTest.Android/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Firebase.Installations;
+using Android.Gms.Common;
+
+namespace AppTest.Droid
+{
+    [Activity(Label = "AppTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity, Android.Gms.Tasks.IOnSuccessListener
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Firebase.FirebaseApp.InitializeApp(this);
+            if (GoogleApiAvailability.Instance
+                .IsGooglePlayServicesAvailable(Application.Context) == ConnectionResult.Success)
+            {
+                FirebaseInstallations.GetInstance(Firebase.FirebaseApp.Instance).GetId().AddOnSuccessListener(this);
+            }
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public void OnSuccess(Java.Lang.Object result)
+        {
+            var a = result.ToString();
+        }
+    }
+}
