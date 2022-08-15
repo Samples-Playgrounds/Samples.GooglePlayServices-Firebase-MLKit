03c2c0b11dbf7932a59f071045c43199051e7975
moljac <mcvjetko@holisticware.net>
2022-08-15 13:33:16 +0200

    mlkit sample added

---
+++ samples/issues-bug-repros/com.google.android.gms/play-services-map/map-sample/App1/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Gms.Maps;
+using Android.OS;
+using AndroidX.AppCompat.App;
+
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
+    {
+        public void OnMapReady(GoogleMap googleMap)
+        {
+
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var mapView = FindViewById<MapView>(Resource.Id.map);
+            mapView.OnCreate(savedInstanceState);
+            mapView.GetMapAsync(this);
+        }
+    }
+}
---
+++ samples/issues-bug-repros/com.google.mlkit/00661-Xamarin.GooglePlayServices.MLKit.Text.Recognition doesnt contain expected APIs/end/App1/App1/MainActivity.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+using Java.Util.Concurrent;
+using Java.Interop;
+
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+
+            Options o = new Options
+            {
+            };
+
+            //Here is the problem
+            var recognizer = Xamarin.Google.MLKit.Vision.Text.TextRecognition.GetClient(o);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (View.IOnClickListener)null).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+
+
+    public class Options : Java.Lang.Object, Xamarin.Google.MLKit.Vision.Text.ITextRecognizerOptionsInterface
+    {
+        public string CreatorClass => throw new NotImplementedException();
+
+        public IExecutor Executor => throw new NotImplementedException();
+
+        public bool IsThickClient => throw new NotImplementedException();
+
+        public int LoggingEventId => throw new NotImplementedException();
+
+        public int LoggingLanguageOption => throw new NotImplementedException();
+
+        public string LoggingLibraryName => throw new NotImplementedException();
+
+        public string ModuleId => throw new NotImplementedException();
+
+        public IntPtr Handle => throw new NotImplementedException();
+
+        public int JniIdentityHashCode => throw new NotImplementedException();
+
+        public JniObjectReference PeerReference => throw new NotImplementedException();
+
+        public JniPeerMembers JniPeerMembers => throw new NotImplementedException();
+
+        public JniManagedPeerStates JniManagedPeerState => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disposed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisposeUnlessReferenced()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Finalized()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetJniIdentityHashCode(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetJniManagedPeerState(JniManagedPeerStates value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPeerReference(JniObjectReference reference)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnregisterFromRuntime()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
